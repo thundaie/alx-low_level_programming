c48d8c6e3f651782c69c46a64927cd7ea054a55f
thundaie <olatundejayeoba@outlook.com>
2022-12-08 15:37:31 -0800

    push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * A c program that uses the printf function
  *
  * return: Always 0 (success)
  */
-int main(void)
-{
+int main(void){
 	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
