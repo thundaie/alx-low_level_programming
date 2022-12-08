d9fea7fb88ec3c6634c9c969a2d0da7657dab2f5
thundaie <olatundejayeoba@outlook.com>
2022-12-08 15:06:56 -0800

    cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * A C program that uses put function
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
