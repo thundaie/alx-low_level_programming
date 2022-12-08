6babd5bad0fbda5376429e10a73eacf8cea479ab
thundaie <olatundejayeoba@outlook.com>
2022-12-08 15:39:09 -0800

    push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,8 @@
  *
  * return: Always 0 (success)
  */
-int main(void){
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
