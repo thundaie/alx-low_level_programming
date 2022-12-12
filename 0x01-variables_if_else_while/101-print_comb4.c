d1b425daaa356a1f1b26149fb6cb492e7dd72037
thundaie <olatundejayeoba@outlook.com>
2022-12-11 16:24:41 -0800

    cee

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Description: possible combinations for 3 digits
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; ++a)
+	{
+		for (b = 49; b < 58; ++b)
+		{
+			for (c = 50; c < 58; ++c)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
